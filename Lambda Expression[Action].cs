e89b763f88a6b65471f304efb8019b8f2330fc2c
Megha-Su <119593325+Megha-Su@users.noreply.github.com>
2022-12-08 12:28:19 +0530

    Add files via upload

---
+++ Lambda Expression[Action].cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Lambda_Expression_Action_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Action<int, int> sumAction =  (firstNumber,secondNumber)=>Console.WriteLine(firstNumber + secondNumber);
+           
+            Action<int, int> subtractAction =  (firstNumber,  secondNumber)=> Console.WriteLine(firstNumber - secondNumber);
+            
+            Action<int, int> multipliationAction =  (firstNumber, secondNumber)=> Console.WriteLine(firstNumber * secondNumber);
+            
+            Action<int, int> divisionAction = (firstNumber, secondNumber)=> Console.WriteLine(firstNumber / secondNumber);
+            
+            Calculator(sumAction, 7, 8);
+            Calculator(subtractAction, 14, 8);
+            Calculator(multipliationAction, 5, 8);
+            Calculator(divisionAction, 16, 8);
+        }
+        static void Calculator(Action<int, int> operation, int number1, int number2)
+        {
+            operation(number1, number2);
+        }
+    }
+}
---
+++ Lambda Expression[Func].cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Lamda_Expression_Action_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Func<int, int, int> addFunc =  (firstNumber,secondNumber) => firstNumber + secondNumber;
+            
+            Func<int, int, int> subtractFunc =  (firstNumber,secondNumber) =>firstNumber - secondNumber;
+            
+            Func<int, int, int> multiplicationFunc =( firstNumber, secondNumber)=>firstNumber * secondNumber;
+            
+            Func<int, int, int> divisionFunc = ( firstNumber,secondNumber)=>firstNumber / secondNumber;
+
+            Calculator(addFunc, 10, 2);
+            Calculator(subtractFunc, 10, 2);
+            Calculator(multiplicationFunc, 10, 3);
+            Calculator(divisionFunc, 10, 5);
+        }
+
+        static void Calculator(Func<int, int, int> operation, int firstNumber, int secondNumber)
+        {
+            Console.WriteLine(operation(firstNumber, secondNumber));
+        }
+    
+    }
+}
